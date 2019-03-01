df93d3a7e938bb164cde0b73ef406835ab165ee8
Evandro Flores <eof@eof.com.br>
2019-03-01 21:41:28 +0000

    init bot

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
 
+	"github.com/shomali11/slacker"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -10,5 +12,15 @@ func main() {
 	token := os.Getenv("PONG_TOKEN")
 	if token == "" {
 		log.Fatal("Bot User OAuth Access Token not found. Set PONG_TOKEN to continue.")
+	}
+
+	bot := slacker.NewClient(token)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal("Could not start the bot", err)
 	}
 }
