d4a318d2e49dce9964bae6a6768e8637e913dae3
Evandro Flores <eof@eof.com.br>
2019-03-04 22:27:01 +0000

    Simulation to check points before playing

---
+++ cmd/simulate.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/evandroflores/udpong/elo"
+	"github.com/evandroflores/udpong/model"
+	"github.com/shomali11/slacker"
+)
+
+func init() {
+	Register("simulate <playerA> vs <playerB>", "Calculate and rank the given Winner and Loser.", simulate)
+}
+
+func simulate(request slacker.Request, response slacker.ResponseWriter) {
+	response.Typing()
+
+	playerAID := cleanID(request.StringParam("playerA", ""))
+	playerBID := cleanID(request.StringParam("playerB", ""))
+
+	if !isUser(playerAID) {
+		response.ReportError(fmt.Errorf("The given winner is not a User"))
+		return
+	}
+
+	if !isUser(playerBID) {
+		response.ReportError(fmt.Errorf("The given loser is not a User"))
+		return
+	}
+
+	playerA, errA := model.GetOrCreatePlayer(playerAID)
+	if errA != nil {
+		response.ReportError(errA)
+		return
+	}
+	playerB, errB := model.GetOrCreatePlayer(playerBID)
+	if errB != nil {
+		response.ReportError(errB)
+		return
+	}
+	simulateA, simulateB := elo.Calc(playerA.Points, playerB.Points)
+	response.Reply(fmt.Sprintf("*%s* wins (%04.f pts) vs *%s* (%04.f pts)", playerA.Name, simulateA, playerB.Name, simulateB))
+
+	simulateB, simulateA = elo.Calc(playerB.Points, playerA.Points)
+	response.Reply(fmt.Sprintf("*%s* wins (%04.f pts) vs *%s* (%04.f pts)", playerB.Name, simulateB, playerA.Name, simulateA))
+}
