e3cd74f9ea36b975b54dcebac23146a4742ab044
Evandro Flores <eof@eof.com.br>
2019-03-16 20:56:54 +0000

    More coverage for cmd.someone_test

--- cmd/someone_test.go
+++ cmd/someone_test.go
@@ -3,21 +3,22 @@ package cmd
 import (
 	"testing"
 
+	"github.com/evandroflores/pong/database"
+	"github.com/evandroflores/pong/model"
 	"github.com/shomali11/proper"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/nlopes/slack"
 )
 
-func TestTryToShowInvalidUser(t *testing.T) {
-	var evt = &slack.MessageEvent{
-		Msg: slack.Msg{
-			Team:    "TTTTTTTTT",
-			Channel: "CCCCCCCCC",
-			User:    "UUUUUUUUU",
-			Text:    "show <@UUUUUUUUU>",
-		}}
+var evt = &slack.MessageEvent{
+	Msg: slack.Msg{
+		Team:    "TTTTTTTTT",
+		Channel: "CCCCCCCCC",
+		User:    "UUUUUUUUU",
+	}}
 
+func TestTryToShowInvalidUser(t *testing.T) {
 	var props = proper.NewProperties(
 		map[string]string{
 			"@someone": "NOTAUSER",
@@ -28,4 +29,47 @@ func TestTryToShowInvalidUser(t *testing.T) {
 
 	someone(request, response)
 	assert.Contains(t, response.GetMessages(), "_Not a User_")
+	assert.Len(t, response.GetMessages(), 1)
+	assert.Empty(t, response.GetErrors())
+}
+
+func TestTryToShowTwoUsersNames(t *testing.T) {
+	var props = proper.NewProperties(
+		map[string]string{
+			"@someone": "USER1234USER123",
+		})
+
+	request := &fakeRequest{event: evt, properties: props}
+	response := &fakeResponse{}
+
+	someone(request, response)
+	assert.Contains(t, response.GetMessages(), "_Not a User_")
+	assert.Len(t, response.GetMessages(), 1)
+	assert.Empty(t, response.GetErrors())
+}
+
+func TestShowValidUser(t *testing.T) {
+	player := model.Player{
+		TeamID:    "TTTTTTTTT",
+		ChannelID: "CCCCCCCCC",
+		SlackID:   "UUUUUUUUU",
+		Name:      "Fake User",
+	}
+
+	database.Connection.Create(&player)
+	defer database.Connection.Unscoped().Where("deleted_at is not null").Delete(&model.Player{})
+	defer database.Connection.Delete(&player)
+
+	var props = proper.NewProperties(
+		map[string]string{
+			"@someone": "UUUUUUUUU",
+		})
+
+	request := &fakeRequest{event: evt, properties: props}
+	response := &fakeResponse{}
+
+	someone(request, response)
+	assert.Contains(t, response.GetMessages(), "*Fake User* has 1000 points (#01)")
+	assert.Len(t, response.GetMessages(), 1)
+	assert.Empty(t, response.GetErrors())
 }
