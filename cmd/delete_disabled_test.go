af9e442893eae276aedde357601b383d37a38551
Evandro Flores <eof@eof.com.br>
2019-09-19 22:15:46 +0100

    Test suite for delete-disabled cmd

---
+++ cmd/delete_disabled_test.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+
+	"github.com/evandroflores/pong/database"
+	"github.com/evandroflores/pong/model"
+	"github.com/evandroflores/pong/slack"
+	ns "github.com/nlopes/slack"
+	"github.com/shomali11/slacker"
+	"github.com/stretchr/testify/suite"
+)
+
+type DeleteDisabledTestSuite struct {
+	suite.Suite
+	channelID   string
+	adminID     string
+	nonAdminID  string
+	players     []model.Player
+	deactivated map[string]model.Player
+}
+
+func (s *DeleteDisabledTestSuite) SetupSuite() {
+	s.channelID = "CTESTDEL"
+	s.adminID = "UIS0ADMIN"
+	s.nonAdminID = "UNOTADMIN"
+	s.players = []model.Player{}
+	s.deactivated = map[string]model.Player{}
+
+	for i := 1; i <= 20; i++ {
+		player := makeTestPlayerWith("TTTTTTTX", s.channelID, fmt.Sprintf("U%08d", i), fmt.Sprintf("Fake User %08d", i))
+		player.Points = 1000 - float64(i)
+		if i%2 == 0 {
+			s.deactivated[player.SlackID] = player
+		}
+		s.players = append(s.players, player)
+
+		database.Connection.Create(&player)
+	}
+
+	mockGetUserInfo := func(sl *slacker.Slacker, id string) (*ns.User, error) {
+		isDeactivated := false
+		if (s.deactivated[id] != model.Player{}) {
+			isDeactivated = true
+		}
+
+		return &ns.User{
+			ID:      id,
+			Deleted: isDeactivated,
+		}, nil
+	}
+
+	monkey.PatchInstanceMethod(reflect.TypeOf(slack.Client), "GetUserInfo", mockGetUserInfo)
+}
+
+func (s *DeleteDisabledTestSuite) TearDownSuite() {
+	for i := 0; i < len(s.players); i++ {
+		database.Connection.Where(&s.players[i]).Delete(&model.Player{})
+	}
+	database.Connection.Unscoped().Where("deleted_at is not null").Delete(&model.Player{})
+}
+
+func TestDeleteDisabledTestSuite(t *testing.T) {
+	suite.Run(t, new(DeleteDisabledTestSuite))
+}
